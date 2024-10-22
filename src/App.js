2a25600fba188f9b06e2efcc6a4bd506fca130a3
JUSTIN SCOTT <JUSTINSCOTT820@GMAIL.COM>
2024-10-22 14:06:49 -0700

    reverting main

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from './Home';
 import Movie from './Movie';
 import './App.css';
-const NotFound = () => {
-  return <h2>404 Not Found</h2>;
-}
+
 const App = () => {
   return (
-    <Router basename="/movie-finder-2.1">
+    <Router>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">Movie Finder</Link>
       </nav>
